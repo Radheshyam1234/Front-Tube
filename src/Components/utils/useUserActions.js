6126e559bde68f2601d9d33468bdd329efa0eaf6
Radheshyam Kr <88738458+Radheshyam1234@users.noreply.github.com>
2022-04-06 20:12:34 +0530

    feat[added like video]

---
+++ src/Components/utils/useUserActions.js
@@ -0,0 +1,62 @@
+import { useStateContext } from "../../Context/StateContext/StateProvider";
+import axios from "axios";
+
+export const useUserActions = () => {
+  const { state, dispatch } = useStateContext();
+
+  const isPresentInLikedVideos = (video) =>
+    state?.likedVideos?.find((item) => item._id == video._id);
+
+  const addToLikedVideos = async (video) => {
+    try {
+      const {
+        data: { likes },
+        status,
+      } = await axios({
+        method: "POST",
+        url: `/api/user/likes`,
+        headers: {
+          authorization: localStorage.getItem("token"),
+        },
+        data: {
+          video,
+        },
+      });
+
+      if (status == 200 || 201) {
+        console.log(likes);
+        dispatch({ type: "SET_LIKED_VIDEOS", payload: likes });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const removeFromLikedVideos = async (videoId) => {
+    console.log("removeFromLikedVideos called", videoId);
+    try {
+      const {
+        data: { likes },
+        status,
+      } = await axios({
+        method: "DELETE",
+        url: `/api/user/likes/${videoId}`,
+        headers: {
+          authorization: localStorage.getItem("token"),
+        },
+      });
+
+      if (status == 200 || 201) {
+        console.log(likes);
+        dispatch({ type: "SET_LIKED_VIDEOS", payload: likes });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return {
+    isPresentInLikedVideos,
+    addToLikedVideos,
+    removeFromLikedVideos,
+  };
+};
