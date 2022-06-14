f0fb51df45eb4d224497fcc9eadd91d1087bc30c
Radheshyam Kr <88738458+Radheshyam1234@users.noreply.github.com>
2022-06-14 08:55:34 +0530

    feat[ added private-router]

--- src/App.js
+++ src/App.js
@@ -4,21 +4,13 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import { useVideosDataProvider } from "./Context/VideosDataContext/VideosDataProvider";
 import { useStateContext } from "./Context/StateContext/StateProvider";
 import { useAuthProvider } from "./Context/AuthContext/AuthProvider";
-import {
-  Home,
-  Navbar,
-  VideoDetailPage,
-  Login,
-  SignUp,
-  LikedVideo,
-  MyProfile,
-  Setting,
-} from "./Components";
+import { Home, Navbar, Login, SignUp, LikedVideo } from "./Components";
 import {
   getVideos,
   getUserProfile,
   getLikedVideos,
 } from "./utilities/backendRequest";
+import { PrivateRoute } from "./Components/PrivateRoute";
 
 import "./styles.css";
 
@@ -43,12 +35,18 @@ export const App = () => {
       <div className="spacer-3rem"></div>
       <Routes>
         <Route path="/" element={<Home />} />
+
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/watch/:id" element={<VideoDetailPage />} />
-        <Route path="/myprofile" element={<MyProfile />}></Route>
-        <Route path="/liked" element={<LikedVideo />}></Route>
-        <Route path="/myprofile/settings" element={<Setting />}></Route>
+
+        <Route
+          path="/liked"
+          element={
+            <PrivateRoute>
+              <LikedVideo />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </div>
   );
---
+++ src/Components/PrivateRoute.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+
+export const PrivateRoute = ({ children }) => {
+  const location = useLocation();
+  const token = localStorage.getItem("token");
+
+  return token ? (
+    children
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
+};
