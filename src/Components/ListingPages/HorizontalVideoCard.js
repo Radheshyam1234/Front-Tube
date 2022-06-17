2d0d2168c594cf7a2285c9cd6b4125393256fd77
Radheshyam Kr <88738458+Radheshyam1234@users.noreply.github.com>
2022-06-17 08:43:28 +0530

    feat[added liked-videos-listing]

---
+++ src/Components/ListingPages/HorizontalVideoCard.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { removeFromPlaylist } from "../../utilities/backendRequest";
+import { useStateContext } from "../../Context/StateContext/StateProvider";
+
+import "./styles.css";
+
+export const HorizontalVideoCard = ({ videoItem, type, playlistId }) => {
+  const { state, dispatch } = useStateContext();
+  const { video, date: timeStamp } = videoItem;
+
+  return (
+    <div className="horizontal-card">
+      <div className="card-horizontal">
+        <Link to={`/watch/${video._id}`}>
+          <div className="card-img">
+            <img
+              loading="lazy"
+              src={video.thumbnail}
+              alt="Nature"
+              className="img-responsive"
+            />
+          </div>
+        </Link>
+
+        <div className="card-details">
+          <p className="text-bold text-medium"> {video.title}</p>
+          <p>
+            <i className="far fa-clock"></i>{" "}
+            <span className="ml-1 text-small  ">
+              {timeStamp.replace("GMT+0530 (India Standard Time)", "")}
+            </span>
+          </p>
+          <div>
+            <p className="text-bold text-small secondary-text-color">
+              {video.channelName}
+            </p>
+          </div>
+        </div>
+
+        <button
+          className="btn"
+          onClick={() => {
+            removeFromPlaylist({
+              dispatch,
+              video,
+              playlistId,
+              type,
+            });
+          }}
+        >
+          <i className="fas fa-trash"></i>
+        </button>
+      </div>
+    </div>
+  );
+};
